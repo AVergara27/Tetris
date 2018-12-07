620d95c75867cb9e43e45916db23899039707e60
Period 1 <Period1@s-cte-20.local>
2018-12-07 08:24:27 -0600

    Added Block.Swift file and Completed BLOCK class...

---
+++ Tetris/Block.swift
@@ -0,0 +1,47 @@
+//
+//  Block.swift
+//  Tetris
+//
+//  Created by Period 1 on 12/7/18.
+//  Copyright © 2018 DGS. All rights reserved.
+//
+
+import SpriteKit
+
+// #1
+let NumberOfColors: UInt32 = 6
+
+// #2
+enum BlockColor: Int, CustomStringConvertible {
+    
+    // #3
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    // #4
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    // #5
+    var description: String {
+        return self.spriteName
+    }
+    
+    // #6
+    static func random() -> BlockColor {
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
