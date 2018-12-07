b6529362a20adf3ddb49a222baf2451851f502fc
Period 1 <Period1@S-CTE-20.local>
2018-12-07 08:20:47 -0600

    Added Array2d

--- Tetris/Array2D.swift
+++ Tetris/Array2D.swift
@@ -5,5 +5,27 @@
 //  Created by Period 1 on 12/7/18.
 //  Copyright © 2018 DGS. All rights reserved.
 //
-
-import Foundation
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(repeating: nil, count:rows * columns)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
